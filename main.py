bedcc6dc9cd601f8e36ca4cb2085308a791f9dd0
Liza Akter <pousheeti123@gmail.com>
2022-08-06 11:06:24 +0000

    password generate

--- main.py
+++ main.py
@@ -1,50 +1,24 @@
 import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+inp_letter=int(input('How many letter you want to add:'))
+inp_number=int(input('How many number you want to add:'))
+inp_symbols=int(input('How many symbol you want to add:'))
 
-rock = '''
-    _______
----'   ____)
-      (_____)
-      (_____)
-      (____)
----.__(___)
-'''
-
-paper = '''
-    _______
----'   ____)____
-          ______)
-          _______)
-         _______)
----.__________)
-'''
-
-scissors = '''
-    _______
----'   ____)____
-          ______)
-       __________)
-      (____)
----.__(___)
-'''
-
-game_images = [rock, paper, scissors]
-
-your_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
-print(game_images[your_choice])
-
-computer_choice = random.randint(0, 2)
-print("Computer chose:")
-print(game_images[computer_choice])
-
-if your_choice >= 3 or your_choice < 0: 
-  print("You typed an invalid number, you lose!") 
-elif your_choice == 0 and computer_choice == 2:
-  print("You win!")
-elif computer_choice == 0 and your_choice == 2:
-  print("You lose")
-elif computer_choice > your_choice:
-  print("You lose")
-elif your_choice > computer_choice:
-  print("You win!")
-elif computer_choice == your_choice:
-  print("It's a draw")
+pass_list=[]
+for i in range(1,inp_letter+1):
+  ran_letter=random.choice(letters)
+  pass_list.append(ran_letter)
+for j in range(1,inp_number+1):
+  pass_list.append(random.choice(numbers))
+for k in range(1,inp_symbols+1):
+  pass_list.append(random.choice(symbols))
+print(pass_list)
+random.shuffle(pass_list)
+print(pass_list)
+strpass=''
+for char in pass_list:
+  strpass+=char
+print(strpass)
+  
